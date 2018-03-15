e9f5f836dc814657b7f7dea41a7c09b2166150e1
Rangga Senatama <ranggasenatama@gmail.com>
2018-03-15 09:45:14 +0700

    test route

--- Startup.cs
+++ Startup.cs
@@ -40,6 +40,9 @@ namespace SIOSR {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "PenggalanganDana",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
